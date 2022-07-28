07dab7dff096899babd5518fcfa5e16ec98e3db0
jindonyy <rsefa94@gmail.com>
2022-07-29 03:44:04 +0900

    Rename: GlobalStyles styles 폴더로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ReservationInfoProvider } from '@contexts/ReservationInfoProvider';
 import { SelectedModalNameProvider } from '@/contexts/SelectedModalNameProvider';
-import GlobalStyles from '@/GlobalStyles';
+import GlobalStyles from '@/styles/GlobalStyles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages';
 
